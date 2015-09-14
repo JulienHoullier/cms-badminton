3b3f35b91072fc9a51e20962d3ba06d5882be9dd
Marc Bouteiller <marcolinho07@gmail.com>
2015-09-14 23:04:49 +0200

    Ajout de joueurs, Mise à jour de la vue avec classements

---
+++ updates/0.0.1-players.js
@@ -0,0 +1,16 @@
+
+exports.create = {
+	Player: [
+		{ 'name.first': 'Marc', 'name.last': 'Bouteiller', email: 'mbouteiller.perso@gmail.com', licence: '06900286', type: 'competitor', timeSlot: 'wednesday_strong' },
+		{ 'name.first': 'Alexandre', 'name.last': 'Marion', email: 'alexandre-marion@hotmail.fr', licence: '06900286', type: 'competitor', timeSlot: 'wednesday_strong' },
+		{ 'name.first': 'Julien', 'name.last': 'Houllier', email: 'j.houllier@gmail.com', licence: '06794300', type: 'competitor', timeSlot: 'wednesday_strong' },
+		{ 'name.first': 'Eva', 'name.last': 'Blouin', email: 'eva.blouin7@gmail.com', licence: '06876011', type: 'competitor', timeSlot: 'wednesday_strong' },
+		{ 'name.first': 'Sandrine', 'name.last': 'Bienassis', email: 'sandrine_bienassis@yahoo.fr', licence: '06664889', type: 'competitor', timeSlot: 'wednesday_strong' },
+		{ 'name.first': 'Sebastien', 'name.last': 'Chotard', email: 'chotard.sebastien@orange.fr', licence: '06755454', type: 'competitor', timeSlot: 'wednesday_strong' }
+	]
+};
+
+// { 'name.first': 'Sylvain', 'name.last': 'Duquesne', email: 'sylvain.duquesne@univ-rennes1.fr', licence: '06900286', type: 'competitor', timeSlot: 'wednesday_strong' },
+// { 'name.first': 'Nathalie', 'name.last': 'Duquesne', email: 'nathalie.duquesne@yahoo.fr', licence: '06900286', type: 'competitor', timeSlot: 'wednesday_strong' },
+// { 'name.first': 'Nicolas', 'name.last': 'Plantis', email: 'nicolas.plantis@gmail.com', licence: '06900286', type: 'competitor', timeSlot: 'wednesday_strong' },
+// { 'name.first': 'Fabien', 'name.last': 'Juguet', email: 'fabjug@hotmail.com', licence: '06900286', type: 'competitor', timeSlot: 'wednesday_strong' },
