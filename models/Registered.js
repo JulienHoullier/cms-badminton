fb8c30d3307b1185113606763f16bf9bddbeaf0e
badboum <alexandre-marion@hotmail.fr>
2015-10-13 23:12:15 +0200

    Add registration state sample

--- models/Registered.js
+++ models/Registered.js
@@ -16,7 +16,8 @@ Registered.add({
     category : {type : Types.Select, options: 'Jeune,Vétérans,Sénior', default: 'Vétérans', required : true, initial : true},
     ranking : {type : Types.Select, options: 'D9,D8,D7,R6,R5,R4,N3,N2,N1', default: 'D9', required : true, initial : true},
     table : {type : Types.Select, options: 'SH,SD,DH,DD,DM', default: 'SH', required : true, initial : true},
-    price : {type : Number, required : true, initial : true}
+    price : {type : Number, required : true, initial : true},
+    state : {type : Types.Select, options: 'En attente, Envoyée, Confirmée, Tournois terminé', default: 'En attente', required : true, initial : true}        //cycle de vie En attente -> Envoyé ->  Confirmé -> Tournois terminé
 });
 
 
