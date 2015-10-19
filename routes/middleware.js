14067d5894d502b51b306a0d29a83b5b6d7242d2
Marc Bouteiller <mbouteiller2.externe@pagesjaunes.fr>
2015-10-19 18:05:05 +0200

    Requete des résultats pour l'index, ajustement CSS

--- routes/middleware.js
+++ routes/middleware.js
@@ -29,7 +29,7 @@ exports.initLocals = function(req, res, next) {
 		{ label: 'Accueil',		 key: 'home',		href: '/' },
 		{ label: 'Actualités',	 key: 'blog',		href: '/blog' },
 		{ label: 'Photos',		 key: 'gallery',	href: '/gallery' },
-		{ label: 'Résultats',	 key: 'results',	href: '/results' }
+		{ label: 'Résultats',	 key: 'results',	href: '/resultats' }
 
 	];
 
@@ -69,12 +69,11 @@ exports.initLocals = function(req, res, next) {
 			});
 		}
 
+		//Contact is the last link
+		locals.navLinks.push({ label: 'Contact',		key: 'contact',		href: '/contact' });
+
 		next(err);
 	});
-	
-	//Contact is the last link
-	locals.navLinks.push({ label: 'Contact',		key: 'contact',		href: '/contact' });
-
 };
 
 
