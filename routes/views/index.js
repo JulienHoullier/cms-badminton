f1cf12a1df6a3af71e60614b3f0da9575bbfe9b3
Marc Bouteiller <mbouteiller2.externe@pagesjaunes.fr>
2015-10-23 10:49:06 +0200

    [Tournois] Ajout du nombre d'inscrit dans les prochains tournois

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,7 +1,8 @@
 var keystone = require('keystone'),
 	Post = keystone.list('Post'),
 	Tournament = keystone.list('Tournament'),
-	Match = keystone.list('Match');
+	Match = keystone.list('Match'),
+	async = require('async');
 	//ffbadnews = require('../../lib/ffbadnews'),
 
 exports = module.exports = function(req, res) {
@@ -28,11 +29,22 @@ exports = module.exports = function(req, res) {
 		.limit(6));
 
 	// Sélection des 5 prochains tournois
-	view.query('tournaments', Tournament.model.find()
+	view.on('init', function(next){
+		Tournament.model.find()
 		.where('registrationDeadLine').gte(today)
 		.sort('date')
-		.limit(5));
-	// TODO : Peupler les registrations
+		.limit(5)
+		.exec(function(err, tournaments){
+			if(err) next(err);
+			async.each(tournaments,function(tournament, next){
+				tournament.populateRelated('registrations', function (err) {
+				next(err);
+				});
+			});
+			locals.tournaments = tournaments;
+			next(err);
+		});
+	});
 
 	// Render the view
 	view.render('newIndex');
