8b0910e411e1f0f99dd7cd70c40f54d88eb3a1a4
JulienHoullier <j.houllier@gmail.com>
2015-11-11 20:00:07 +0100

    add young player for connected user

--- routes/views/player.js
+++ routes/views/player.js
@@ -11,19 +11,9 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'player';
 
-	view.on('init', function(next) {
-
-		// Load the players by sortOrder
-		keystone.list('Player').model.find({type: { $ne: 'young' }}).populate('team').sort('name').exec(function (err, results){
-
-			if (err || !results.length) {
-				return next(err);
-			}
-
-			locals.players = results;
-
-			// Ajout des classements aux joueurs
-			async.each(locals.players, function (player, next){
+	var fillPLayers= function (players, next){
+		// Ajout des classements aux joueurs
+			async.each(players, function (player, next){
 				classement(player.licence, function(err, data){
 					if(!err && data && data.length == 3){
 						player.ranking = data[0] + " / " + data[1] + " / " + data[2];	
@@ -34,10 +24,23 @@ exports = module.exports = function(req, res) {
 				});
 			},
 			function (err){
+				console.log('passe ici');
 				next(err);
 			});
-		});
+	}
+	// Load the players by sortOrder
+	view.query('players', keystone.list('Player').model.find({type: { $ne: 'young' }}).populate('team').sort('name')).then(function(err, players, next){
+			if (err) return next(err);
+			fillPLayers(players, next);
 	});
+	
+	if(req.user){
+		// Load the young players by sortOrder
+		view.query('youngsters', keystone.list('Player').model.find({type: { $eq: 'young' }}).sort('name')).then(function(err, players, next){
+				if (err) return next(err);
+				fillPLayers(players, next);
+		});
+	}
 
 	// Render the view
 	view.render('player');
