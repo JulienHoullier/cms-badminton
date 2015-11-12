376096d7f824218810bba9e5e1de5c867a2dc403
Marc Bouteiller <mbouteiller2.externe@pagesjaunes.fr>
2015-11-12 17:47:27 +0100

    [InscriptionTournoi] Enregistrement de l'inscription

--- routes/views/tournoi/inscription.js
+++ routes/views/tournoi/inscription.js
@@ -15,6 +15,7 @@ exports = module.exports = function(req, res) {
 		idTournoi: req.params.idTournoi
 	};
 
+    // Récupération du tournoi
 	view.on('render', function(next){
 		Tournament.model.findOne({ _id: locals.filters.idTournoi }).exec(function(err, result) {
 				locals.tournoi = result;
@@ -22,14 +23,36 @@ exports = module.exports = function(req, res) {
 			});
 	});
 	
-	view.on('render', function(next){
+    // Récupération du joueur lié à l'utilisateur
+	view.on('init', function(next){
 		req.user.populate('player', function(err, user){
-			locals.playerName = user.player.name.full;
+			locals.player = user.player;
 			next(err);
 			});
 	});
 
-    view.query('listJoueur2', Player.model.find());
+    // Sélection des joueurs pour le partenaire
+    view.query('listJoueur2', Player.model.find().sort('name'));
+
+    view.on('post', function(next) {
+        var newRegistration = new Registration.model();
+        newRegistration.tournament = locals.filters.idTournoi;
+        var updater = newRegistration.getUpdateHandler(req);
+        
+        updater.process(req.body, {
+            flashErrors: true,
+            fields: 'player1, player2, ranking, category, message',
+            errorMessage: "Problème lors de l'enregistrement de l'inscription"
+        }, function(err) {
+            if (err) {
+                locals.validationErrors = err.errors;
+            } else {
+                console.log(newRegistration);
+                locals.registrationSubmitted = true;
+            }
+            next();
+        });
+    });
 
 	view.render("tournoi/inscription");
 }
