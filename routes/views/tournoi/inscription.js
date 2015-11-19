6d1404c7cf81933bf88f2bfddef10cc796871c33
Marc Bouteiller <mbouteiller2.externe@pagesjaunes.fr>
2015-11-19 17:58:08 +0100

    [InscriptionTournois] Redirection sur la page des tournois en cas de succès

--- routes/views/tournoi/inscription.js
+++ routes/views/tournoi/inscription.js
@@ -15,6 +15,26 @@ exports = module.exports = function(req, res) {
 		idTournoi: req.params.idTournoi
 	};
 
+    view.on('post', function(next) {
+        var newRegistration = new Registration.model();
+        newRegistration.tournament = locals.filters.idTournoi;
+        var updater = newRegistration.getUpdateHandler(req);
+
+        updater.process(req.body, {
+            flashErrors: true,
+            fields: 'player1, player2, ranking, category, message',
+            errorMessage: "Problème lors de l'enregistrement de l'inscription..."
+        }, function(err) {
+            if (err) {
+                locals.validationErrors = err.errors;
+                next();
+            } else {
+                req.flash('success', "Votre inscription a bien été prise en compte et est en attente de confirmation !");
+    			res.redirect('/tournois');
+            }
+        });
+    });
+
     // Récupération du tournoi
 	view.on('render', function(next){
 		Tournament.model.findOne({ _id: locals.filters.idTournoi }).exec(function(err, result) {
@@ -22,9 +42,9 @@ exports = module.exports = function(req, res) {
 				next(err);
 			});
 	});
-	
+
     // Récupération du joueur lié à l'utilisateur
-	view.on('init', function(next){
+	view.on('render', function(next){
 		req.user.populate('player', function(err, user){
 			locals.player = user.player;
 			next(err);
@@ -32,27 +52,13 @@ exports = module.exports = function(req, res) {
 	});
 
     // Sélection des joueurs pour le partenaire
-    view.query('listJoueur2', Player.model.find().sort('name'));
-
-    view.on('post', function(next) {
-        var newRegistration = new Registration.model();
-        newRegistration.tournament = locals.filters.idTournoi;
-        var updater = newRegistration.getUpdateHandler(req);
-        
-        updater.process(req.body, {
-            flashErrors: true,
-            fields: 'player1, player2, ranking, category, message',
-            errorMessage: "Problème lors de l'enregistrement de l'inscription"
-        }, function(err) {
-            if (err) {
-                locals.validationErrors = err.errors;
-            } else {
-                console.log(newRegistration);
-                locals.registrationSubmitted = true;
-            }
-            next();
-        });
+    view.on('render', function(next){ 
+    	Player.model.find().sort('name').exec(function(err, result){
+    		locals.listJoueur2 = result;
+    		next(err);
+    	});
     });
 
 	view.render("tournoi/inscription");
+
 }
