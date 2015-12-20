491e580347ac3fd3511637d9504f74f215b5c867
Marc Bouteiller <marcolinho07@gmail.com>
2015-12-20 15:25:45 +0100

    [Twitter] Possibilité de tweeter après avoir publié

--- models/Post.js
+++ models/Post.js
@@ -78,8 +78,8 @@ Post.schema.post('save', function() {
     }
 });
 Post.schema.post('save', function(post) {
-    if (this.stateModified && this.state == 'published' && !this.socialized ) {
-    	// Tweet si le statut passe à "published" et que l'article n'a pas été publié sur les réseaux sociaux.
+    if (this.stateModified && this.state == 'published' || this.socialIsModified && this.socialVisible && !this.socialized ) {
+    	// Tweet si le statut passe à "published" ou que l'article n'a pas été publié sur les réseaux sociaux.
     	this.populate('author category', function (err, post){
 			var status = buildTweet(post.title, post.author.name.first, post.slug, post.category.name);
 			twitterClient.tweet(status, function(error){
