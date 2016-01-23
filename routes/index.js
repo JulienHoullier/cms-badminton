978df3169cf907b1c3c0d2c8d67f4c5f7821168e
JulienHoullier <j.houllier@gmail.com>
2016-01-23 02:31:16 +0100

    Add comments, add, remove functionality in post detail

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ exports = module.exports = function(app) {
 	// Views
 	app.get('/', routes.views.index);
 	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
+	app.all('/blog/post/:post', routes.views.post);
 	app.get('/pages/:page', routes.views.page);
 	app.get('/gallery', routes.views.gallery);
 	app.get('/tournois', routes.views.tournoi.tournois);
