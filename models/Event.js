22c644e84d5d297206a9bd9bd4ae9927ab23cb5f
Marc Bouteiller <marcolinho07@gmail.com>
2016-09-12 21:32:09 +0200

    Event : add a weekly event

--- models/Event.js
+++ models/Event.js
@@ -17,6 +17,8 @@ Event.add({
 	date: { type: Types.Date, format: "DD-MM-YYYY", required: true, initial:true },
 	startHour: { type: Types.Text, required: true, initial:true, label: "Heure de début", note:"exemple: 20h30" },
 	endHour: { type: Types.Text, required: true, initial:true, label: "Heure de fin", note:"exemple: 22h30" },
+	weekly: { type: Types.Boolean, required: false, initial:true, label: "Hebdomadaire" },
+	endDate: { type: Types.Date, required: true, initial:true, label: "Date de fin", dependsOn:{weekly:true} },
 	canceled: { type: Types.Boolean, required: false, initial:false, label: "Annuler", note:"L'évènement sera barré." }
 });
 
@@ -26,6 +28,29 @@ Event.hasRoles = function(user){
 	}
 	return false;
 };
+
+// Enregistrement Hebdomadaire jusqu'à la date de fin.
+Event.schema.post('save', function() {
+		var testDate = new Date(this.date);
+		testDate.setDate(testDate.getDate() + 7);
+		console.log(testDate);
+    if (this.weekly && testDate <= this.endDate) {
+			var newDate = this.date;
+			newDate.setDate(newDate.getDate() + 7);
+			var event = new Event.model({
+				description: this.description,
+				date : newDate,
+				startHour: this.startHour,
+				endHour: this.endHour,
+				endDate: this.endDate,
+				weekly: true
+			});
+			// réutilise le post-save
+			event.save(function(err){
+				console.log(err);
+			});
+    }
+});
 
 Event.defaultSort = '-date';
 Event.defaultColumns = 'name, date, description';
